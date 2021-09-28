afe9d589542c6a975ca13a99cde412277b0b04e0
Matthew Logan <matthew.logan@accenture.com>
2021-09-28 14:23:51 +0000

    Split APP views and controllers

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -7,6 +7,25 @@ sap.ui.define([
 		return Controller.extend("loginapp.controller.Master", {
 			onInit: function () {
 
-			}
+            },
+            
+            goToPersonal: function() {
+                var oApp = this.getView().getParent().getParent();
+                oApp.toDetail("idApp--idSplitApp--idPersonal", "baseSlide");
+            },
+
+            goToExperience: function() {
+                var oApp = this.getView().getParent().getParent();
+                oApp.toDetail("idApp--idSplitApp--idExperience", "baseSlide");;
+            },
+
+            goToAddress: function() {
+                var oApp = this.getView().getParent().getParent();
+                oApp.toDetail("idApp--idSplitApp--idAddress", "baseSlide");
+            },
+
+            onNavBack: function () {
+                window.location.href = "./index.html";
+            }
 		});
 	});
